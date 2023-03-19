b384b2065e074cce13b714765a8cbaca3acea2ee
Mario <galla@mutably.io>
2023-03-19 17:24:53 +0100

    new email template id

--- pages/api/email/index.ts
+++ pages/api/email/index.ts
@@ -1,30 +1,29 @@
 import sendgrid from "@sendgrid/mail";
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 import { IContactFormValues } from "../../../types";
 
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY as string);
 
 async function sendEmail(req: NextApiRequest, res: NextApiResponse) {
-    try {
-        const formValues = req.body as IContactFormValues
+  try {
+    const formValues = req.body as IContactFormValues;
 
+    const t = await sendgrid.send({
+      to: formValues.email,
+      from: "info@mutably.io",
+      bcc: ["info@mutably.io", "galla@mutably.io"],
+      templateId: "d-5b4d7dc284ea4a06aff9a6ff11d364ef",
+      dynamicTemplateData: {
+        ...formValues,
+      },
+    });
+    console.log("email sent", t);
+  } catch (error) {
+    console.log("error", error.response.body);
+    return res.status(500).json({ error: "Mail Delivery failed" });
+  }
 
-        await sendgrid.send({
-            to: formValues.email,
-            from: "info@mutably.io",
-            bcc: ["info@mutably.io", "galla@mutably.io"],
-            templateId: "d-3cd29e8234b54725a11e347acc535bb6",
-            dynamicTemplateData: {
-                ...formValues,
-            }
-
-        });
-
-    } catch (error) {
-        return res.status(500).json({ error: "Mail Delivery failed" });
-    }
-
-    return res.status(200).json({ error: "" });
+  return res.status(200).json({ error: "" });
 }
 
 export default sendEmail;
